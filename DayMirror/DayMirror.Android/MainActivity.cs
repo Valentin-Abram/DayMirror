cc712a737d36e970a982506b04cad84c74f0f40d
Valentin Abram <abram.valentin@outlook.com>
2019-09-08 11:10:37 +0300

    Release

--- DayMirror/DayMirror.Android/MainActivity.cs
+++ DayMirror/DayMirror.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DayMirror.Droid
 {
-    [Activity(Label = "DayMirror", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Mirror", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
